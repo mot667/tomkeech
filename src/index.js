507e7e01445ac1ecdfc343bc40334bdf469209f8
Tom Keech <45048917+mot667@users.noreply.github.com>
2020-07-22 23:13:44 -0400

    changed router

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(   
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>, document.getElementById('root'));
